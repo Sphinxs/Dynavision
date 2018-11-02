c47b7c53066c1fa1c1b5a3be59069c4101e2d8ae
Walter <waltim@outlook.com>
2018-11-02 18:38:27 -0300

    Save dataset and plot in different lines

--- classify_images/l04_e01.py
+++ classify_images/l04_e01.py
@@ -26,6 +26,10 @@ from sklearn.tree import DecisionTreeClassifier
 
 from sklearn.ensemble import RandomForestClassifier
 
+from sklearn.gaussian_process import GaussianProcessClassifier
+
+from sklearn.gaussian_process.kernels import RBF
+
 from json import dumps
 
 # Load M Peg 7
@@ -68,8 +72,7 @@ for className, imagesDict in mpeg7.items():
                     prop.eccentricity,
                     prop.mean_intensity,
                     prop.solidity,
-                    (4 * pi * prop.area) /
-                    prop.perimeter ** float(2)  # Circularity
+                    (4 * pi * prop.area) / prop.perimeter ** float(2)  # Circularity
                 ]
             })
 
@@ -79,6 +82,8 @@ for className, imagesDict in mpeg7.items():
 
 mpeg7Data = np.vstack(mpeg7DataCache)
 
+
+
 lp = preprocessing.LabelEncoder()
 
 lp.fit(mpeg7LabelsCache)
@@ -87,9 +92,19 @@ mpeg7Labels = lp.transform(mpeg7LabelsCache)
 
 mpeg7LabelsNames = lp.classes_
 
+# Save dataset
+
+np.savetxt(
+    'mpeg7.txt',
+    np.c_[mpeg7Data.copy(), mpeg7Labels],
+    delimiter=',',
+    fmt= '%10.3f',
+    header='Area, eccentricity, mean_intensity, solidity, circularity, label'
+)
+
 # Split dataset
 
-x_train, x_test, y_train, y_test = model_selection.train_test_split( # Cross_val_score
+x_train, x_test, y_train, y_test = model_selection.train_test_split(  # Cross_val_score
     mpeg7Data,
     mpeg7Labels,
     test_size=0.25
@@ -98,7 +113,7 @@ x_train, x_test, y_train, y_test = model_selection.train_test_split( # Cross_val
 # Normal distribution
 #
 # www.scipy-lectures.org/intro/scipy/auto_examples/plot_normal_distribution.html
-# 
+#
 # www.tutorialspoint.com/python/python_normal_distribution.htm
 #
 # docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.random.normal.html
@@ -138,6 +153,16 @@ ax.set_title('Normal Distribution')
 
 predicted = {}
 
+# Gaussian Process Classifier
+
+gaussian = GaussianProcessClassifier(1.0 * RBF(1.0))
+
+gaussian.fit(x_train, y_train)
+
+predicted.update({
+    'Gaussian Process Classifier': gaussian.predict(x_test)  # Y test
+})
+
 # Knn
 
 knn = KNeighborsClassifier(n_neighbors=8)
@@ -145,7 +170,7 @@ knn = KNeighborsClassifier(n_neighbors=8)
 knn.fit(x_train, y_train)
 
 predicted.update({
-    'K-Nearest Neighbors': knn.predict(x_test) # Y test
+    'K-Nearest Neighbors': knn.predict(x_test)  # Y test
 })
 
 # Naive Bayes
@@ -155,7 +180,7 @@ bayes = GaussianNB()
 bayes.fit(x_train, y_train)
 
 predicted.update({
-    'Naive Bayes': bayes.predict(x_test) # Y Test
+    'Naive Bayes': bayes.predict(x_test)  # Y Test
 })
 
 # Decision Tree
@@ -165,7 +190,7 @@ tree = DecisionTreeClassifier(max_depth=5)
 tree.fit(x_train, y_train)
 
 predicted.update({
-    'Decision Tree': tree.predict(x_test) # Y Test
+    'Decision Tree': tree.predict(x_test)  # Y Test
 })
 
 # Random Forest
@@ -175,7 +200,7 @@ random = RandomForestClassifier()
 random.fit(x_train, y_train)
 
 predicted.update({
-    'Random Forest': random.predict(x_test) # Y
+    'Random Forest': random.predict(x_test)  # Y
 })
 
 # Svm
@@ -185,36 +210,73 @@ svm = svm.SVC()
 svm.fit(x_train, y_train)
 
 predicted.update({
-    'Suport Vector Machine': svm.predict(x_test) # Y Test
+    'Suport Vector Machine': svm.predict(x_test)  # Y Test
 })
 
 # Get metrics and plot confusion matrix
 
-fig, ax = pp.subplots(1, len(predicted), figsize=(len(predicted) * 2, 3))
+
+def plot(ax, row, figs):
+    '''
+        Plot n figures in a specific row
+
+        Parameters
+        ----------
+
+        ax:
+            Subplot ax to plot the figures
+
+        row:
+            Row to plot the figures
+
+        figs:
+            List of figures (key, value)
+
+        Returns
+        -------
+
+        Return a list of metrics from the figures received by the function
+
+        Usage
+        -----
+
+        >>> None
+    '''
+
+    counter = 0
+
+    cache = {}
+
+    for k, v in figs:
+        confusion = metrics.confusion_matrix(y_test, v)
+
+        sn.heatmap(confusion, annot=True, ax=ax[row, counter])
+
+        ax[row, counter].set_title(k)
+
+        counter += 1
+
+        cache.update({
+            k: {
+                'accuracy': metrics.accuracy_score(y_test, v),
+                'jaccard_similarity': metrics.jaccard_similarity_score(y_test, v)
+            }
+        })
+
+    return cache
+
+
+fig, ax = pp.subplots(2, 3, figsize=(9, 6))
 
 predictedMetrics = {}
 
-counter = 0
+predictedMetrics.update(
+    plot(ax, 0, list(predicted.items())[:3])
+)
 
-for k, v in predicted.items():
-    confusion = metrics.confusion_matrix(y_test, v)
-    
-    sn.heatmap(confusion, annot=True, ax=ax[counter])
-
-    ax[counter].set_title(k)
-    
-    # print(
-    #    f'\n{k} - \033[34mReport\033[m:\n\n{metrics.classification_report(y_test, v)}', end='\n'
-    # )
-
-    predictedMetrics.update({
-        k: {
-            'accuracy_score': metrics.accuracy_score(y_test, v),
-            'jaccard_similarity_score': metrics.jaccard_similarity_score(y_test, v)
-        }
-    })
-
-    counter += 1
+predictedMetrics.update(
+    plot(ax, 1, list(predicted.items())[3:7])
+)
 
 pp.show()
 
@@ -225,7 +287,7 @@ print(dumps(
         reversed(
             sorted(
                 predictedMetrics.items(),
-                key=lambda item: item[1]['accuracy_score']
+                key=lambda item: item[1]['accuracy']
             )
         )
     ),
